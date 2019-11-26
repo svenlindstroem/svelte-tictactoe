1f1b54509a51382f31686e68c4346c0243026f34
Sven Lindstroem <svenlindstroem@gmail.com>
2019-11-26 11:09:33 +0100

    adding more chached files

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,18 @@
-var cacheName = "TicTacToe";
+var cacheName = "TicTacToe-1";
 var filesToCache = [
   "/",
-  "/index.html"
-  /*"/global.css",
-  "/css/style.css",
-  "/js/main.js"*/
+  "/index.html",
+  "/global.css",
+  "/build/bundle.css",
+  "/build/bundle.js",
+  "/manifest.json",
+  "favicon-32x32.png",
+  "favicon-16x16.png"
 ];
-alert(1);
+
 /* Start the service worker and cache all of the app's content */
 self.addEventListener("install", function(e) {
-  console.log("hello");
+  console.log("service worker install");
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
