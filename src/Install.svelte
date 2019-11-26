c35645bb7ae1898a17db6b410f1cac9a07e89d5e
Sven Lindstroem <svenlindstroem@gmail.com>
2019-11-26 10:37:30 +0100

    Added Install.svelte, rm site.manifest and replaced with manifest.json, install button working

---
+++ src/Install.svelte
@@ -0,0 +1,42 @@
+<script>
+  "use strict";
+
+  let deferredInstallPrompt = null;
+  let installButton;
+
+  // could use onMount here as well
+  document.addEventListener("DOMContentLoaded", event => {
+    installButton = document.getElementById("buttonInstall");
+    installButton.addEventListener("click", installPWA);
+  });
+
+  window.addEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
+  window.addEventListener("appinstalled", logAppInstalled);
+
+  function saveBeforeInstallPromptEvent(evt) {
+    deferredInstallPrompt = evt;
+    installButton.removeAttribute("hidden");
+  }
+
+  function installPWA(evt) {
+    deferredInstallPrompt.prompt();
+    // Hide the install button, it can't be called twice.
+    evt.srcElement.setAttribute("hidden", true);
+    deferredInstallPrompt.userChoice.then(choice => {
+      if (choice.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt", choice);
+      } else {
+        console.log("User dismissed the A2HS prompt", choice);
+      }
+      deferredInstallPrompt = null;
+    });
+  }
+
+  function logAppInstalled(evt) {
+    console.log("Weather App was installed.", evt);
+  }
+</script>
+
+<div>
+  <button id="buttonInstall" aria-label="Install" hidden>Install</button>
+</div>
